baa60091cbd129ba08e37fda251cbb7957bba45c
Timothy Tao <timctao@gmail.com>
2016-08-25 13:15:53 -0700

    Add more interview questions to the program

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,4 +4,8 @@ vampire_name = gets.chomp
 puts "What year were you born?"
 vampire_age = gets.to_i
 
-puts "Our company"
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health plan? (y/n)"
+health_plan = gets.chomp
