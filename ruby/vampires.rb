165d07c067fd98c7d813f92869276e80db6d39cc
Timothy Tao <timctao@gmail.com>
2016-08-25 18:20:57 -0700

    Add true/false identifiers for all questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,6 +1,12 @@
 puts "What is your name?"
 vampire_name = gets.chomp
 
+	if vampire_name == "Alucard" || vampire_name == "Edward Cullen" || vampire_name == "Anne Rice"
+		vampire_name = true
+	else
+		vampire_name = false
+	end
+
 puts "What year were you born?"
 
 	begin
@@ -8,6 +14,12 @@ puts "What year were you born?"
 	rescue
 		puts "Please enter a valid year"
 		retry
+	end
+
+	if vampire_age <= 1916 || vampire_age >=2016
+		vampire_age = true
+	else
+		vampire_age = false
 	end
 	
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
@@ -18,10 +30,22 @@ garlic_bread = gets.chomp
 		garlic_bread = gets.chomp	
 	end
 
+	if garlic_bread == "n"
+		garlic_bread = true
+	else
+		garlic_bread = false
+	end
+
 puts "Would you like to enroll in the company's health plan? (y/n)"
 health_plan = gets.chomp
 
 	until health_plan == "y" || health_plan == "n"
 		puts "Please enter valid response (y/n)"
 		health_plan = gets.chomp
+	end
+
+	if health_plan == "n"
+		health_plan = true
+	else
+		health_plan = false
 	end
