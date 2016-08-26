23a6407d988674f5c78a109a874c7295c693c42b
Timothy Tao <timctao@gmail.com>
2016-08-25 17:46:47 -0700

    Add conditional loop to garlic bread question

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -13,5 +13,10 @@ puts "What year were you born?"
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 garlic_bread = gets.chomp
 
+	until garlic_bread == "y" || garlic_bread == "n"
+		puts "Please enter valid response (y/n)"
+		garlic_bread = gets.chomp	
+	end
+
 puts "Would you like to enroll in the company's health plan? (y/n)"
 health_plan = gets.chomp
