e77db194bea1bc8e4be7da48d0af5745c8f3866b
Timothy Tao <timctao@gmail.com>
2016-08-27 12:10:37 -0700

    Add pseudocode comments for program interface

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -29,19 +29,23 @@ alphabet = "abcdefghijklmnopqrstuvwxyz"
 	input
 end
 
+# Ask user to Encrypt or Decrypt a passcode
 puts "Encrypt or Decrypt?"
 answer = gets.chomp
 
+# Make it so that they must type "Encrypt" or "Decrypt" to continue
 	until answer == "Encrypt" || answer == "Decrypt"
 		puts "Invalid answer, try again"
 		answer = gets.chomp
 	end
 
+# Once choice is made, run the correct program
 	if answer == "Encrypt"
 
 	puts "Type in passcode"
 	passcode = gets.chomp
 
+# Print result as encrypted string
 	p encrypt(passcode)
 
 	end
@@ -51,6 +55,7 @@ answer = gets.chomp
 	puts "Type in coded phrase"
 	locked_phrase = gets.chomp.downcase
 
+# Print result as decrypted string
 	p decrypt(locked_phrase)
 
 	end
