05398473e6a1e19bf637df26c8a1ab065d4a3b73
Timothy Tao <timctao@gmail.com>
2016-08-29 21:14:29 -0700

    Add .ord to display ascii code for the letter z

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -32,7 +32,7 @@
 " suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
 # "The mystery of the missing first letter".<???>
@@ -45,5 +45,5 @@
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count "a"
 # => 4
