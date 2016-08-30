ef0a12eb7adf02a6ec772f15748951492a84e0a3
Timothy Tao <timctao@gmail.com>
2016-08-30 10:54:04 -0400

    Add questions for name and date of birth and assign them to the hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -2,3 +2,13 @@
 # Store all user input information into a hash
 # Once all information has been entered, print current hash state
 # If user wants to change anything, allow them to input which key to change until "none" is typed
+
+designer = {}
+
+puts "Hello! Thank you for applying. Please answer the following questions:"
+
+puts "What is your name?"
+designer[:name] = gets.chomp
+
+puts "What is your DOB?"
+designer[:DOB] = gets.chomp
