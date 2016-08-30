e6f5333a11bc9893b14fe679a7aa5707a7afd7df
Timothy Tao <timctao@gmail.com>
2016-08-30 10:49:42 -0400

    Add pseudocode to plan out algorithm

---
+++ ruby/hashes.rb
@@ -0,0 +1,4 @@
+# Ask user for some basic information, such as name, experience, and date of birth
+# Store all user input information into a hash
+# Once all information has been entered, print current hash state
+# If user wants to change anything, allow them to input which key to change until "none" is typed
