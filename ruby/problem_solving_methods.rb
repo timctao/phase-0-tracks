de5e8648cf08d451699f967f33118c52ae5d30a8
Timothy Tao <timctao@gmail.com>
2016-09-01 17:10:04 -0400

    Comment in other methods for final submission, cleaned up excess code and comments

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -9,26 +9,18 @@ arr = [5, 12, 9, 20]
 # p search_array(arr, 9)
 # => [2]
 
-# Release 1
-
-# def  fibonacci(n)
-#     n1,n2=0,1
-#     while n1 <=n
-#          print "#{n1}"
-#         n1,n2=n2,n1+n2
-#     end
-# end
-# fibonacci(10)
+# Release 1 - Fibonacci
 
 def fibonacci( n )
 	fib = [0, 1]
-    (2..n).each do |n| 
+    (2..n).each do |n| - 1 
     next_fib = fib[ n - 1 ] + fib[ n - 2 ]
     fib.push(next_fib)
 	end
 	p fib
 end 
-fibonacci( 100 )
+fibonacci( 6 )
+# => [0, 1, 1, 2, 3, 5, 8]
 
 
 
@@ -40,22 +32,22 @@ fibonacci( 100 )
 # End loop once all values are evaluated to be in ascending order.
 # Print swapped array 
 
-# def bubble_sort(array)
-#   n = array.length
-#   loop do
-#     swapped = false
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swapped = false
 
-#     (n-1).times do |i|
-#       if array[i] > array[i+1]
-#         array[i], array[i+1] = array[i+1], array[i]
-#         swapped = true
-#       end
-#     end
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
 
-#     break if not swapped
-#   end
+    break if not swapped
+  end
 
-# array  
-# end
-# example = [1, 6, 2, 7, 4]
-# p bubble_sort(example)
+array  
+end
+example = [1, 6, 2, 7, 4]
+p bubble_sort(example)
