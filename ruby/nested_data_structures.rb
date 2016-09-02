d66e69df064d0add6edfae1f3fb0b0652abbdf36
Timothy Tao <timctao@gmail.com>
2016-09-02 17:40:52 -0400

    Add first layer of nested arrays and hashes

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,1 +1,19 @@
 # Release 2: Nested Data Structures - Classroom
+
+classroom = {
+	teacher_desk = [
+
+	],
+	first_row = {
+
+	},
+	second_row = {
+
+	},
+	third_row = {
+
+	},
+	supply_closet = {
+		
+	}
+}
