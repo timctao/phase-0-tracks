f1b9fe334ca8f76258a888ebf5ccc8a237563cc8
Timothy Tao <timctao@gmail.com>
2016-09-02 17:54:01 -0400

    Add second layer of nested data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -5,15 +5,42 @@ classroom = {
 
 	],
 	first_row: {
+		left_desk: {
 
+		},
+		middle_desk: {
+
+		}, 
+		right_desk: {
+
+		}
 	},
 	second_row: {
+		left_desk: {
 
+		},
+		middle_desk: {
+
+		},
+		right_desk: {
+
+		}
 	},
 	third_row: {
+		left_desk: {
 
+		},
+		middle_desk: {
+
+		},
+		right_desk:{
+
+		}
 	},
 	supply_closet: {
-
+		top_shelf: [
+		], 
+		bottom_shelf: [
+		]
 	}
 }
