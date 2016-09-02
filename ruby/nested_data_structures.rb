888b949f6f211d88635f06c6e917a6629b459ac0
Timothy Tao <timctao@gmail.com>
2016-09-02 18:31:57 -0400

    Add items to will_desk

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -6,30 +6,33 @@ classroom = {
 	],
 	first_row: {
 		will_desk: {
-
+			name_tag: 'Will Byers',
+			book: 'Lord of the Rings',
+			folder: ["drawings", "classwork"],
+			miscellaneous: ["black pen", "pencil", "dice bag"]
 		},
 		lucas_desk: {
-
+			name_tag: 'Lucas Sinclair'
 		}, 
 		dustin_desk: {
-
+			name_tag: 'Dustin Henderson'
 		},
 		mike_desk: {
-
+			name_tag: 'Mike Wheeler'
 		}
 	},
 	second_row: {
 		eleven_desk: {
-
+			name_tag: 'Eleven'
 		},
 		nancy_desk: {
-
+			name_tag: 'Nancy Wheeler'
 		},
 		barb_desk: {
-
+			name_tag: 'Barbara Holland'
 		},
 		jonathan_desk: {
-
+			name_tag: 'Jonathan Wheeler'
 		}
 	},
 	supply_closet: {
