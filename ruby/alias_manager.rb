dc12e568c0dedfe811d256580d4c51d6e3352f49
Timothy Tao <timctao@gmail.com>
2016-09-04 22:13:38 -0400

    Clean up shovel operator lines and combine them to one push command

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,7 +1,6 @@
 def name_swap(first_name, last_name)
 	code_name = []
-	code_name << first_name
-	code_name << last_name
+	code_name.push(first_name, last_name)
 	code_name[0], code_name[1] = code_name[1], code_name[0]
 	code_name.join(" ")
 end
