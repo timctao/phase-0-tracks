7c3f6b4b0c15ffa99d92c641135cbb784b6c875f
Timothy Tao <timctao@gmail.com>
2016-09-05 00:32:54 -0400

    Add converters for vowels and consonants

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,8 +1,37 @@
 def name_swap(first_name, last_name)
 	code_name = []
 	code_name.push(first_name, last_name)
-	code_name[0], code_name[1] = code_name[1], code_name[0]
-	code_name.join(" ")
+	code_name.reverse!.join(' ')
 end
 
-p name_swap("Felicia", "Torres")
+def vowel_converter(swapped_name)
+	code_name = swapped_name.downcase.chars
+	vowels = %w{a e i o u}
+	code_name.map! do |letter|
+		if letter == "u"
+			letter = "a"
+		elsif vowels.include?(letter)
+			vowels[vowels.index(letter) + 1]
+		else
+			letter
+		end
+	end
+	code_name.join('')
+end
+
+def consonant_converter(swapped_name)
+	code_name = swapped_name.chars
+	consonants = %w{b c d f g h j k l m n p q r s t v w x y z}
+	code_name.map! do |letter|
+		if letter == "z"
+			letter = "b"
+		elsif consonants.include?(letter)
+			consonants[consonants.index(letter) + 1]
+		else
+			letter
+		end
+	end
+	code_name.join('')
+end
+
+p consonant_converter(vowel_converter(name_swap("Felicia", "Torres")))
