1c36201828671253a9d8a2abc14e2d6025f83157
Timothy Tao <timctao@gmail.com>
2016-09-07 13:20:15 -0400

    Add more instance methods to Puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -14,7 +14,18 @@ class Puppy
   end
 
   def dog_years(int)
-  	
+  	human_age = int * 7
+  	p human_age
+  end
+
+  def play_dead
+	puts "Say bang!"
+	input = gets.chomp
+	if input == "bang!"
+		p "*plays dead*"
+	else
+		p "*Dog looks at you funny*"
+	end
   end
 
 end
@@ -24,3 +35,7 @@ puppy = Puppy.new.fetch("train")
 bark = Puppy.new.speak(4)
 
 trick = Puppy.new.roll_over
+
+age = Puppy.new.dog_years(3)
+
+new_trick = Puppy.new.play_dead
