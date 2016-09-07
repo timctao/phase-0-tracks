aaa177727bfd5fb9dec34d676ee6322a2e858c39
Timothy Tao <timctao@gmail.com>
2016-09-07 10:48:48 -0400

    Add puppy class characteristics

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+SPECIES ------------------------
+Starkus Direwolfus
+
+CHARACTERISTICS ----------------
+Size: very large
+Friendly: depends
+Eye count: 2
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Jump
+Attack
+Growl
