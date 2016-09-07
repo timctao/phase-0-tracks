384c5614f7d363474fe64964232eeedec947c72f
Timothy Tao <timctao@gmail.com>
2016-09-07 13:15:24 -0400

    Add more instance methods for Puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,8 +5,22 @@ class Puppy
     toy
   end
 
+  def speak(int)
+  	int.times { puts "Woof!" }
+  end
+
+  def roll_over
+  	p "*rolls over*"
+  end
+
+  def dog_years(int)
+  	
+  end
+
 end
 
 puppy = Puppy.new.fetch("train")
 
+bark = Puppy.new.speak(4)
 
+trick = Puppy.new.roll_over
