237c0d1381c43ac48f544d89c68bf3fdcbe78162
Timothy Tao <timctao@gmail.com>
2016-09-09 01:24:19 -0400

    Add initialize and two other instance methods to the Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,3 +1,15 @@
 class Santa
-	
+
+	def initialize
+		puts "Initializing Santa instance..."
+	end
+
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!"
+	end
+
 end
