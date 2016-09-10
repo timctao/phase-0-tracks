0ebb97263ba2e1a3f8509c4afffbe405906f42c9
Timothy Tao <timctao@gmail.com>
2016-09-10 16:30:25 -0400

    Add new methods and getter/setter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -18,13 +18,39 @@ class Santa
 		puts "That was a good #{cookie}!"
 	end
 
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at(reindeer)
+		@reindeer_ranking.delete_at(@reindeer_ranking.index(reindeer))
+		@reindeer_ranking.insert(-1, reindeer)		
+	end
+
+	# Getter Methods
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	# Setter Methods
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
 end
 
-santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
 
-santas[0].speak
+santa = Santa.new("male", "asian")
+
+
+p santa.get_mad_at("Rudolph")
