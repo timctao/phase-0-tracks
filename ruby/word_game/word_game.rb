69c45953dcc9f2fa1c7b349f265b57f5c1785476
Timothy Tao <timctao@gmail.com>
2016-09-11 22:35:13 -0400

    Pseudocode game design to prepare for coding

---
+++ ruby/word_game/word_game.rb
@@ -0,0 +1,9 @@
+# Create a word game that starts with user 1 inputting any word.
+# Display to user 2 blank spaces using underscores the length of the word
+# Display to user 2 the number of guesses remaining, number of guesses should equal the length of the word
+# Let user 2 guess the word by typing any letter of the alphabet.
+# If the letter guessed is in the word, replace the blank letter with the guessed letter. Guess count goes down by 1
+# If the letter is not in the word, then answer with "Try another letter". Guess count goes down by 1
+# If the letter guessed is a repeat, answer with "You already guessed that letter, try another". Guess count remains the same
+# If user 2 manages to complete the word before guess counter reaches 0, display congratulations message
+# If user 2 does not manage to complete the word before guess counter reaches 0, display taunting message
