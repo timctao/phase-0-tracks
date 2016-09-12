b8af9cc9f782e86bf44cee316b46fa1f86fbe06c
Timothy Tao <timctao@gmail.com>
2016-09-12 11:43:15 -0400

    Deleted extra code, cleaned up descriptions

--- ruby/word_game/word_game_spec.rb
+++ ruby/word_game/word_game_spec.rb
@@ -7,17 +7,9 @@ describe Hangman do
 		expect(game.word).to eq ["t", "e", "s", "t"]
 	end
 
-	# it "replaces the input word with blank underscores" do
-	# 	expect(game.convert_word).to eq ["_", "_", "_", "_"]
-	# end
-
-	it "displays current guess count" do
+	it "displays current guess count limit to length of inputted word" do
 		expect(game.guess_count).to eq 4
 	end
-
-	# it "displays the guessed letter in the hint array" do 
-	# 	expect(game.guess_letter("t")).to eq "t _ _ t"
-	# end	
 
 	it "stores letter guesses to the correct arrray" do
 		expect(game.guess_letter("e")).to eq ["e"]		
