16eca14e014b938c75fab662d806fdd0d9407a45
Timothy Tao <timctao@gmail.com>
2016-09-19 00:43:02 -0400

    Create function to compare two key value pairs

--- js/algos.js
+++ js/algos.js
@@ -10,6 +10,22 @@ function longest_phrase(array) {
 	return (longest_phrase)
 }
 
+// Release 1: Write a function that compares two values
+
+function match_pair(input_1, input_2) {
+	match_pair = null
+	if (input_1.name == input_2.name) {
+		match_pair = true
+	} else if (input_1.age == input_2.age) {
+		match_pair = true
+	} else {
+		match_pair = false
+	}
+	return match_pair
+}
+
+// Driver code
+
 var array = [
 	"long phrase",
 	"longest phrase",
