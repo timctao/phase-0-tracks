f43f5a02d904c2b3333011e0e9001cf9a97513ce
Timothy Tao <timctao@gmail.com>
2016-09-26 09:19:04 -0400

    Create information tables for storing calorie content in calories.db

--- ruby/calorie_counter/calorie_counter.rb
+++ ruby/calorie_counter/calorie_counter.rb
@@ -3,4 +3,34 @@ require 'sqlite3'
 # Create database for calorie counting
 db = SQLite3::Database.new("calories.db")
 
-# Creates tables for each 
+# Creates info tables in calorie database
+create_meat_table = <<-SQL
+	CREATE TABLE IF NOT EXISTS meats (
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		servings INT,
+		calories INT
+	)
+SQL
+
+create_dairy_table = <<-SQL
+	CREATE TABLE IF NOT EXISTS dairy (
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		servings INT,
+		calories INT
+	)
+SQL
+
+create_fruits_veggies_table = <<-SQL
+	CREATE TABLE IF NOT EXISTS fruits_veggies (
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		servings INT,
+		calories INT
+	)
+SQL
+
+db.execute(create_meat_table)
+db.execute(create_dairy_table)
+db.execute(create_fruits_veggies_table)
