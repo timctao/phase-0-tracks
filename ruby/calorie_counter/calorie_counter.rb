25fd3a829c5ce462eb9f1e1a3e5b83782293c32d
Timothy Tao <timctao@gmail.com>
2016-09-26 09:04:14 -0400

    Delete all databases, recreate singular database

--- ruby/calorie_counter/calorie_counter.rb
+++ ruby/calorie_counter/calorie_counter.rb
@@ -1,7 +1,6 @@
 require 'sqlite3'
 
-# Create multiple databases for calorie counting
-meat = SQLite3::Database.new("meats.db")
-dairy_db = SQLite3::Database.new("dairy.db")
-fruits_veggies_db = SQLite3::Database.new("fruits_veggies.db")
-calorie_counter_db = SQLite3::Database.new("calorie_counter.db")
+# Create database for calorie counting
+db = SQLite3::Database.new("calorie_counter.db")
+
+# Creates tables for each 
